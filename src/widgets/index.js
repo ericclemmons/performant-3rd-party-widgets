4d106a2909e1f6a97745915da05fff1e784cf82c
Eric Clemmons <eric@smarterspam.com>
2017-05-03 15:58:09 -0500

    Add code-splitting

--- src/widgets/index.js
+++ src/widgets/index.js
@@ -1,27 +1,40 @@
 import { camelCase } from "lodash";
-import chat from "./chat";
-import hello from "./hello";
 
-const widgets = { chat, hello };
+(async () => {
+  const containers = [...document.querySelectorAll("[data-my-widget]")];
 
-const containers = [...document.querySelectorAll("[data-my-widget]")];
+  for (const container of containers) {
+    const options = [...container.attributes]
+      .filter(({ name }) => /^data-/.test(name))
+      .reduce((acc, attribute) => {
+        const { name, value } = attribute;
+        const prop = camelCase(name.replace(/^data-(.+$)/, "$1"));
 
-for (const container of containers) {
-  const options = [...container.attributes]
-    .filter(({ name }) => /^data-/.test(name))
-    .reduce((acc, attribute) => {
-      const { name, value } = attribute;
-      const prop = camelCase(name.replace(/^data-(.+$)/, "$1"));
+        return {
+          ...acc,
+          [prop]: value
+        };
+      }, {});
 
-      return {
-        ...acc,
-        [prop]: value
-      };
-    }, {});
+    const { myWidget, ...props } = options;
 
-  const { myWidget, ...props } = options;
+    // Attempt to import widget init script, or throw exception		+  const render = widgets[myWidget];
+    const render = await (async () => {
+      try {
+        const imported = await import(`./${myWidget}.js`);
 
-  const render = widgets[myWidget];
+        // `import`ed code-split modules contain all exports
+        return imported.default;
+      } catch (e) {
+        // Friendly error message for invalid widget names
+        if (e.message.match("Cannot find module")) {
+          throw new Error(`Unknown data-my-widget: ${myWidget}`);
+        } else {
+          throw e;
+        }
+      }
+    })();
 
-  render(props, container);
-}
+    render(props, container);
+  }
+})();
