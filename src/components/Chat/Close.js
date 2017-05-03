eb455101a17f970b9d9b6c881f1202253fb17e8a
Chad Donohue <chad.donohue@gmail.com>
2017-05-03 16:54:04 -0500

    Added styled-jsx

--- src/components/Chat/Close.js
+++ src/components/Chat/Close.js
@@ -1,26 +1,33 @@
 import color from "color";
 import React from "react";
-import styled from "styled-components";
-
-const Button = styled.button`
-  background: ${props => props.buttonColor}
-  border: 1px solid transparent;
-  border-radius: 100%;
-  box-shadow: 0 2px 7px rgba(0, 0, 0, 0.2);
-  color: ${props => color(props.buttonColor).luminosity() > 0.6 ? "#222" : "white"}
-  cursor: pointer;
-  font-size: 16px;
-  position: absolute;
-  right: -15px;
-  top: -15px;
-  transition: all 0.5s cubic-bezier(0.165, 0.84, 0.44, 1);
-  z-index: 99999;
-
-  &:hover {
-    border: 1px solid ${props => (color(props.buttonColor).luminosity() > 0.6 ? "#222" : "white")}
-  }
-`;
 
 export default (props) => (
-  <Button {...props} onClick={props.onClick}>×</Button>
-)
+  <button 
+    className="root"
+    style={{
+      background: props.buttonColor,
+      color: color(props.buttonColor).luminosity() > 0.6 ? "#222" : "white",
+    }}
+    onClick={props.onClick} 
+  >
+    ×
+    <style jsx>{`
+      .root {
+        border: 1px solid transparent;
+        border-radius: 100%;
+        box-shadow: 0 2px 7px rgba(0, 0, 0, 0.2);
+        cursor: pointer;
+        font-size: 16px;
+        position: absolute;
+        right: -15px;
+        top: -15px;
+        transition: all 0.5s cubic-bezier(0.165, 0.84, 0.44, 1);
+        z-index: 99999;
+
+        &:hover {
+          border: 1px solid white;
+        }
+      }
+    `}</style>
+  </button>
+);
